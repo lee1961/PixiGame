5d72e66bd62a6b52f036b38714655cad290da55b
lee1961 <lee1961@purdue.edu>
2016-12-28 04:57:51 -0500

    connection from sockets now work

--- index.js
+++ index.js
@@ -9,9 +9,23 @@ app.get('/',function(req,res) {
   //res.sendFile(__dirname + '/index.html');
   res.render('/index.html');
 });
-
+var count = 0;
 io.on('connection',function(socket) {
-  console.log("A user has connected");
+  console.log("A user has connectedbbb  ");
+
+  socket.on('connect', function(data) {
+    //socket.emit('success',"successfull")
+    //console.log(data);
+    console.log("roger that");
+  });
+  socket.on('clientEvent', function(data) {
+    //socket.emit('success',"successfull")
+    //console.log(data);
+    console.log("roger that");
+  });
+  socket.on('success',function(data) {
+    console.log("hey");
+  });
 
   socket.on('disconnect',function() {
     console.log("a user has disconnected");
