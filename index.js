4022c2acd3cba40d1c5363da9074096dcc4ffd6e
lee1961 <lee1961@purdue.edu>
2016-12-28 07:19:09 -0500

    comitting something bout particular given ID

--- index.js
+++ index.js
@@ -10,14 +10,35 @@ app.get('/',function(req,res) {
   res.render('/index.html');
 });
 var count = 0;
+var player_1 = "player1";
+var socket_1;
+var socket_2;
+var player_2 = "player2";
 io.on('connection',function(socket) {
-  console.log("A user has connectedbbb  ");
+  count++;
+  socket.emit('uniqueID',count);
+
+  console.log("A user has connected ");
 
   socket.on('connect', function(data) {
     //socket.emit('success',"successfull")
     //console.log(data);
+
     console.log("roger that");
   });
+
+  socket.on('socketID',function(data) {
+      //console.log("the socketID of the client is" + data.socketID);
+      if(count == 1) {
+        socket_1 = data.socketID;
+        socket.emit('givenID',player_1);
+      } else if (count == 2) {
+          socket_2 = data.socketID;
+          socket.emit('givenID',player_2);
+      }
+
+  });
+
   socket.on('clientEvent', function(data) {
     //socket.emit('success',"successfull")
     //console.log(data);
@@ -28,6 +49,7 @@ io.on('connection',function(socket) {
   });
 
   socket.on('disconnect',function() {
+    count--;
     console.log("a user has disconnected");
   });
 
