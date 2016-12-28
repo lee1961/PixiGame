6d84cbcfe173562eaa725096c46ad33d3d44bc5f
lee1961 <lee1961@purdue.edu>
2016-12-28 18:28:46 -0500

    fixed a tiny bug

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/static'));
 
 app.get('/',function(req,res) {
-  //res.sendFile(__dirname + '/index.html');
-  res.render('/index.html');
+    //res.sendFile(__dirname + '/index.html');
+    res.render('/index.html');
 });
 var count = 0;
 var player_1 = "player1";
@@ -15,48 +15,54 @@ var socket_1;
 var socket_2;
 var player_2 = "player2";
 io.on('connection',function(socket) {
-  count++;
-  socket.emit('uniqueID',count);
+    count++;
+    socket.emit('uniqueID',count);
 
-  console.log("A user has connected ");
+    console.log("A user has connected ");
 
-  socket.on('connect', function(data) {
-    //socket.emit('success',"successfull")
-    //console.log(data);
+    socket.on('connect', function(data) {
+        //socket.emit('success',"successfull")
+        //console.log(data);
 
-    console.log("roger that");
-  });
+        console.log("roger that");
+    });
 
-  socket.on('socketID',function(data) {
-      //console.log("the socketID of the client is" + data.socketID);
-      if(count == 1) {
-        socket_1 = data.socketID;
-        socket.emit('givenID',player_1);
-      } else if (count == 2) {
-          socket_2 = data.socketID;
-          socket.emit('givenID',player_2);
-      }
+    socket.on('givenSocketID',function(data) {
+        //console.log("the socketID of the client is" + data.socketID);
+        if(count == 1) {
+            socket_1 = data.socketID;
+            socket.emit('givenID',player_1);
+        } else if (count == 2) {
+            socket_2 = data.socketID;
+            socket.emit('givenID',player_2);
+        }
 
-  });
+    });
 
-  socket.on('clientEvent', function(data) {
-    //socket.emit('success',"successfull")
-    //console.log(data);
-    console.log("roger that");
-  });
-  socket.on('success',function(data) {
-    console.log("hey");
-  });
+    socket.on('clientEvent', function(data) {
+        //socket.emit('success',"successfull")
+        //console.log(data);
+        console.log("roger that");
+    });
+    socket.on('success',function(data) {
+        console.log("hey");
+    });
 
-  socket.on('disconnect',function() {
-    count--;
-    console.log("a user has disconnected");
-  });
+    socket.on('disconnect',function() {
+        count--;
+        console.log("a user has disconnected");
+    });
+
+
+    socket.on('up_press', function(data) {
+        sock = data;
+
+    });
 
 });
 
 
 
 http.listen(3000, function() {
-  console.log("listening on port 3000");
+    console.log("listening on port 3000");
 });
